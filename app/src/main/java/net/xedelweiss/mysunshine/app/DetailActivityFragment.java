db982db6a70af1e945554d19595df792b8d969d2
Michael Sverdlikovsky <michael.sverdlikovsky@ab-soft.net>
2016-01-06 14:54:50 +0200

    Lesson 3 is done (share)

--- app/src/main/java/net/xedelweiss/mysunshine/app/DetailActivityFragment.java
+++ app/src/main/java/net/xedelweiss/mysunshine/app/DetailActivityFragment.java
@@ -3,7 +3,13 @@ package net.xedelweiss.mysunshine.app;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v4.view.MenuItemCompat;
+import android.support.v7.widget.ShareActionProvider;
+import android.util.Log;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
@@ -13,7 +19,12 @@ import android.widget.TextView;
  */
 public class DetailActivityFragment extends Fragment {
 
+    private static final String LOG_TAG = DetailActivityFragment.class.getSimpleName();
+    private static final String FORECAST_SHARE_HASH_TAG = "#MySunshineApp";
+    private String mForecastStr;
+
     public DetailActivityFragment() {
+        setHasOptionsMenu(true);
     }
 
     @Override
@@ -23,10 +34,33 @@ public class DetailActivityFragment extends Fragment {
         Intent intent = getActivity().getIntent();
 
         if (intent != null && intent.hasExtra(Intent.EXTRA_TEXT)) {
-            String forecast = intent.getExtras().getString(Intent.EXTRA_TEXT);
-            ((TextView)rootView.findViewById(R.id.detail_text)).setText(forecast);
+            mForecastStr = intent.getExtras().getString(Intent.EXTRA_TEXT);
+            ((TextView)rootView.findViewById(R.id.detail_text)).setText(mForecastStr);
         }
 
         return rootView;
+    }
+
+    @Override
+    public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
+        inflater.inflate(R.menu.detailfragment, menu);
+        MenuItem shareMenuItem = menu.findItem(R.id.action_share);
+        ShareActionProvider mShareActionProvider = (ShareActionProvider) MenuItemCompat.getActionProvider(shareMenuItem);
+
+        if (mShareActionProvider != null) {
+            mShareActionProvider.setShareIntent(createShareForecastIntent());
+        } else {
+            Log.d(LOG_TAG, "Share Action Provider is null!");
+        }
+    }
+
+    private Intent createShareForecastIntent() {
+        Intent shareIntent = new Intent(Intent.ACTION_SEND);
+
+        shareIntent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_WHEN_TASK_RESET);
+        shareIntent.setType("text/plain");
+        shareIntent.putExtra(Intent.EXTRA_TEXT, mForecastStr + " " + FORECAST_SHARE_HASH_TAG);
+
+        return shareIntent;
     }
 }
